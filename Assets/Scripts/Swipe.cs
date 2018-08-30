70d415fc1daec83dfa9194fda726e1aecaf67b69
DanylUnity3D <devandrienko@gmail.com>
2018-08-30 16:49:56 +0300

    Primitive Swime Logic & Controls

---
+++ Assets/Scripts/Swipe.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using UnityEditor.Experimental.UIElements;
+using UnityEngine;
+
+public class Swipe : MonoBehaviour
+{
+    private bool tap, swipeLeft, swipeRight, swipeUp, swipeDown;
+    private bool isDragging;
+    private Vector2 startTouch, swipeDelta;
+
+    public bool Tap => tap;
+    public Vector2 SwipeDelta => swipeDelta;
+    public bool SwipeLeft => swipeLeft;
+    public bool SwipeRight => swipeRight;
+    public bool SwipeUp => swipeUp;
+    public bool SwipeDown => swipeDown;
+
+    private void Update()
+    {
+        tap = swipeLeft = swipeRight = swipeUp = swipeDown = false;
+
+        #region Standalone Inputs
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            tap = true;
+            isDragging = true;
+            startTouch = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+            Reset();
+        }
+
+        #endregion
+
+        #region Mobile Inputs
+
+        if (Input.touches.Length != 0)
+        {
+            switch (Input.touches[0].phase)
+            {
+                case TouchPhase.Began:
+                    tap = true;
+                    isDragging = true;
+                    startTouch = Input.touches[0].position;
+                    break;
+                case TouchPhase.Ended:
+                case TouchPhase.Canceled:
+                    isDragging = false;
+                    Reset();
+                    break;
+                case TouchPhase.Moved:
+                    break;
+                case TouchPhase.Stationary:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        #endregion
+
+        // Calculate the distance
+        swipeDelta = Vector2.zero;
+        if (isDragging)
+        {
+            if (Input.touches.Length != 0)
+            {
+                swipeDelta = Input.touches[0].position - startTouch;
+            }
+            else if (Input.GetMouseButton(0))
+            {
+                swipeDelta = (Vector2) Input.mousePosition - startTouch;
+            }
+        }
+
+        // If we cross the dead zone
+        if (swipeDelta.magnitude > 125)
+        {
+            // Which direction?
+            float x = swipeDelta.x;
+            float y = swipeDelta.y;
+            if (Mathf.Abs(x) > Mathf.Abs(y))
+            {
+                // Left or right
+                if (x < 0)
+                {
+                    swipeLeft = true;
+                }
+                else
+                {
+                    swipeRight = true;
+                }
+            }
+            else
+            {
+                // Up or down
+                if (y < 0)
+                {
+                    swipeDown = true;
+                }
+                else
+                {
+                    swipeUp = true;
+                }
+
+                Reset();
+            }
+        }
+    }
+
+    private void Reset()
+    {
+        startTouch = swipeDelta = Vector2.zero;
+        isDragging = false;
+    }
+}
---
+++ Assets/Scripts/SwipeControl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class SwipeControl : MonoBehaviour
+{
+    [SerializeField] private Swipe swipeLogic;
+    private Vector3 destination;
+
+    private void Update()
+    {
+        if (swipeLogic.SwipeLeft)
+        {
+            destination += Vector3.left;
+        }
+
+        if (swipeLogic.SwipeRight)
+        {
+            destination += Vector3.right;
+        }
+
+        if (swipeLogic.SwipeUp)
+        {
+            destination += Vector3.forward;
+        }
+
+        if (swipeLogic.SwipeDown)
+        {
+            destination += Vector3.back;
+        }
+
+        gameObject.transform.position =
+            Vector3.MoveTowards(gameObject.transform.position, destination, 2f * Time.deltaTime);
+
+        if (swipeLogic.Tap)
+        {
+            Debug.Log("Tap!");
+        }
+    }
+}
